911fe32a9a4aa2b2531ef19d90f2283ac97e19e6
Jefferj <jefferj080@gmail.com>
2023-03-10 16:59:47 -0500

    Project setup

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Static Files
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// Home
+app.get('/', (req, res) => {
+
+    const locals = {
+        title: 'Hospital-system',
+        description: 'I am a description'
+    }
+    res.render('index', locals);
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
